afcb5f0e3da9751d0b0f22d7f0a5e2bbf472f5c2
Musa Muhammed Awwal <talk2muhammedawwal@gmail.com>
2026-08-05 19:10:35 +0200

    fix sup

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -10,7 +10,8 @@ export const authorize = (requiredRoles: string | string[]) => {
     const allowedRoles = Array.isArray(requiredRoles)
       ? requiredRoles
       : [requiredRoles];
-    const hasRole = allowedRoles.includes(user.role);
+    const hasRole =
+      allowedRoles.includes(user.role) || user.role === "SUPER_ADMIN"; // SUPER_ADMIN has access to all routes
     if (!hasRole) {
       return next(
         new ApiError(
